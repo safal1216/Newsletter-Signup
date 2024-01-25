a57916e12dc3c6ba16458f9648b0e37615689de8
Safal Mehrotra <safalmehrotra1216@gmail.com>
2024-01-25 23:11:40 +0530

    files create

---
+++ app.js
@@ -0,0 +1,13 @@
+// const express=require("express");
+// const Parse=require("body-parser");
+// const req=require("request");
+// const app=express();
+
+// app.use(Parse.urlencoded({extended : true}));
+
+// app.get("/",(req,res)=>{
+//     res.sendFile(__dirname + "/index.html");
+// });
+
+// app.listen(3000,() => 
+//     {console.log("using localhost 3000")});
