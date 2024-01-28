1a1c68725bb6e97b4fd941e8d0bc73db7e95853a
Safal Mehrotra <safalmehrotra1216@gmail.com>
2024-01-28 21:40:58 +0530

    updates done

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-// const express=require("express");
-// const Parse=require("body-parser");
-// const req=require("request");
-// const app=express();
+const express=require("express");
+const Parse=require("body-parser");
+const req=require("request");
+const app=express();
 
-// app.use(Parse.urlencoded({extended : true}));
+app.use(express.static("public"));
+app.use(Parse.urlencoded({extended : true}));
 
-// app.get("/",(req,res)=>{
-//     res.sendFile(__dirname + "/index.html");
-// });
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+});
 
-// app.listen(3000,() => 
-//     {console.log("using localhost 3000")});
+app.post("/",(req,res)=>{
+    let firnam=req.body.Firstname;
+    let lasnam=req.body.Lastname;
+    let email=req.body.email;
+    console.log(firnam, lasnam, email);
+});
+
+app.listen(3000,() => 
+    {console.log("using localhost 3000")});
