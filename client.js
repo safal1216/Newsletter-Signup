90630d1e4a2baa4c541d72afa1297e90343e851d
Safal Mehrotra <safalmehrotra1216@gmail.com>
2024-01-31 00:39:03 +0530

    corrections done

--- client.js
+++ client.js
@@ -1,11 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const submitButton = document.getElementById('navigateButton');
-
-    submitButton.addEventListener('click', function(event) {
+const submitButton = document.getElementById('navigateButton');
+submitButton.addEventListener('click', function(event) {
         // Prevent the default form submission behavior
         event.preventDefault();
 
         // Redirect to the to-do list page
         window.location.href = '/todo';
     });
-});
