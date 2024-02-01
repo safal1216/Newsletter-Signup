4b1dc304a2fda2ae6163f202dae421fafb7b947a
Safal Mehrotra <safalmehrotra1216@gmail.com>
2024-02-01 17:24:08 +0530

    change done

--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 const submitButton = document.getElementById('navigateButton');
 submitButton.addEventListener('click', function(event) {
-        // Prevent the default form submission behavior
         event.preventDefault();
 
         // Redirect to the to-do list page
